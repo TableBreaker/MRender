0c9bcbfa84692f8c1dc24e774439dee6f7517789
luyuanpeng <karexy@qq.com>
2019-11-28 20:30:09 +0800

    push code

--- MRender/MRender/MRender.h
+++ MRender/MRender/MRender.h
@@ -1,3 +1,20 @@
 #pragma once
 
 #include "resource.h"
+#include "framework.h"
+
+class MRender
+{
+public:
+	MRender();
+	~MRender();
+	bool Initialize();
+	bool UpdateFrame();
+	void Shutdown();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+
+};
+
